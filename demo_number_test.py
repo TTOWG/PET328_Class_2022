7c6f0cdfd661f696d80949770bc53e82e1d5b7a1
TTOWG <olatunde.mosobalaje@covenantuniversity.edu.ng>
2022-04-21 17:18:39 +0100

    Updates lecture slides; add demos and algorithm

---
+++ demo_number_test.py
@@ -0,0 +1,10 @@
+x = input('Tell me the number')
+x = float(x)
+if x>0:
+    print('Number is positive')
+    print('Congrats')
+else:
+    print('Oops! Number is negative')
+    print('Try again')
+        
+print('Have a nice day')
