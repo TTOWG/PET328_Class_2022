222ab9ae092b6b38a7533d3e2c500b5023eb09a3
Toluwalase Akanbi 19CN026124 <toluwalase.akanbi@stu.cu.edu.ng>
2022-05-03 21:22:24 +0000

    Update bayes_classifier.py

--- bayes_classifier.py
+++ bayes_classifier.py
@@ -1,20 +1,19 @@
-#Computing For Dolomite
+#unconditional probability that the interval is Dolomite or Shale
 dcc = float(input('Number of Dolomite Cores: '))
-ndc = float(input('Number of all cores: '))
+dcs = float(input('Number of shale cores: '))
+ndc = dcc+dcs
 PD = dcc/ndc
+PS = dcs/ndc
+#conditional probability of obtaining a gamma log reading greater than 60 in the Dolomite and Shale lithology
 loc = float(input('Mean of Dolomite Data: '))
 scale = float(input('Standard Deviation of Dolomite Data: '))
+loc_S = float(input('Mean of Shale Data: '))
+scale_S = float(input('Standard Devition of Shale data: '))
 import scipy.stats
-PodD60 = 1-scipy.stats.norm(loc,scale).cdf(0.5)
-#Computing For Shale
-dcs = float(input('Number of shale cores: '))
-PS = dcs/ndc
-loc = float(input('Mean of Shale Data: '))
-scale = float(input('Standard Devition of Shale data: '))
-import scipy.stats
-podS60 = 1-scipy.stats.norm(loc,scale).cdf(0.5)
-#gamma log reading is greater than 60
-PlD_gamma60 = (PD*PodD60)/(PD*PodD60+PS*podS60)
+PodD60 = 1-scipy.stats.norm(loc,scale).cdf(60)
+podS60 = 1-scipy.stats.norm(loc_S,scale_S).cdf(60)
+#Baye’s Theorem to classify intervals in the formation as either as Dolomite or Shale
+PlD_gamma60 = (PD*PodD60)/((PD*PodD60)+(PS*podS60))
 #Classifying The Intervals
 if PlD_gamma60 >= 0.5:
     print('The Interval is Dolomite.')
