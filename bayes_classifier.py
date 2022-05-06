8d3e283ad4dcff8d6a15810be3066e22e93d6e06
Oreoluwa Leo-Dada 19CN026158 <102850595+OREOLUWALEO-DADA@users.noreply.github.com>
2022-05-06 19:11:00 +0200

    Add files via upload

---
+++ bayes_classifier.py
@@ -0,0 +1,31 @@
+Num_of_dolo= float(input('Enter number of dolomite cores: '))
+Num_of_Shale= float(input('Enter number of Shale cores: '))
+
+Mean_dolo= float(input('Enter the mean of dolomite: '))
+Mean_Shale= float(input('Enter the mean of Shale: '))
+
+Dev_dolo= float(input('Enter the standard Dev of dolomite: '))
+Dev_Shale= float(input('Enter the standard Dev of Shale: '))
+
+Total = Num_of_dolo + Num_of_Shale
+P_D = Num_of_dolo/Total
+P_S = Num_of_Shale/Total
+
+print('P(D) =',P_D)
+print('P(S) =',P_S)
+
+import scipy.stats
+
+p_gamma_greaterthan_60_D = 1 - scipy.stats.norm(Mean_dolo, Dev_dolo).cdf(60)
+p_gamma_greaterthan_60_S = 1 - scipy.stats.norm(Mean_Shale, Dev_Shale).cdf(60)
+
+print('P(gamma>60(D) =',p_gamma_greaterthan_60_D)
+print('P(gamma>60(S) =',p_gamma_greaterthan_60_S)
+
+P_D_GAMMA_GREATERTHAN_60 = ((P_D + p_gamma_greaterthan_60_D)/((P_D * p_gamma_greaterthan_60_D)+\
+                                                             (P_S * p_gamma_greaterthan_60_S)))
+
+if P_D_GAMMA_GREATERTHAN_60 >= 0.5:
+    print('The interval is dolomite')
+else:
+    print('It is a Shale interval')
